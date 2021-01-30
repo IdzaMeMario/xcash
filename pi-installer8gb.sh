782be927ce851189b169411d9f5c77e8d6269e19
IdzaMeMario <51014889+IdzaMeMario@users.noreply.github.com>
2021-01-30 01:24:09 -0500

    Update pi-installer8gb.sh

--- pi-installer8gb.sh
+++ pi-installer8gb.sh
@@ -2397,9 +2397,9 @@ function backup()
   if [ "${SHARED_DELEGATE^^}" == "YES" ]; then
     cd ~
     systemctl start mongodb
-    mongodump --db XCASH_PROOF_OF_STAKE_DELEGATES &>/dev/null
-    7z a shared_delegates_database_backup.7z dump &>/dev/null
-    sudo rm -r dump &>/dev/null  
+    mongodump --db XCASH_PROOF_OF_STAKE_DELEGATES &>/dev/null || true
+    7z a shared_delegates_database_backup.7z dump &>/dev/null || true
+    sudo rm -r dump &>/dev/null || true
   fi
 
   echo
@@ -2410,7 +2410,6 @@ function backup()
 
   # Display the decentralized database backup data
   if [ "${SHARED_DELEGATE^^}" == "YES" ]; then
-    ls ~
     echo
     echo
     echo -e "${COLOR_PRINT_YELLOW}After running the autoinstaller on a different machine run this command to import your shared delegates database (place the shared_delegates_database_backup.7z in the $HOME directory)${END_COLOR_PRINT}" 
